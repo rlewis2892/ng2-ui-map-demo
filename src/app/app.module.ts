804ae4c1083838faca6737ad3868be8cf570f244
Rochelle Lewis <rlewis37@gmail.com>
2017-12-11 14:13:58 -0700

    import NguiMapModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {JwtModule} from "@auth0/angular-jwt";
 
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
+import {NguiMapModule} from "@ngui/map";
 
 const moduleDeclarations = [AppComponent];
 
@@ -22,7 +23,7 @@ const JwtHelper = JwtModule.forRoot({
 });
 
 @NgModule({
-	imports:      [BrowserModule, HttpClientModule, ReactiveFormsModule, routing, JwtHelper],
+	imports:      [BrowserModule, HttpClientModule, ReactiveFormsModule, routing, JwtHelper, NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyBXxCP6BYTsZfvNMSPafFno5po23pe5CCk'})],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [...appRoutingProviders]
